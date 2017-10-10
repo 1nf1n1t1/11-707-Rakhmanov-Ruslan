55e6c735fe725ec818f865d9e0886bbab78755a9
1nf1n1t1 <ruslan-3100@mail.com>
2017-10-10 17:58:08 +0300

    *

---
+++ Part1_2/Part1_2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Part1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startPosition = Console.ReadLine();
+            var finishPosition = Console.ReadLine();
+            int stNum = int.Parse(startPosition[1].ToString());
+            int fshNum = int.Parse(finishPosition[1].ToString());
+
+            if (startPosition[0] == finishPosition[0])
+            {
+                if (Math.Abs(stNum - fshNum) == 1 && (stNum != 1 || stNum != 8)) Console.WriteLine("Yes");
+                else if (Math.Abs(stNum - fshNum) == 2 && (stNum == 2 || stNum == 7)) Console.WriteLine("Yes");
+                else Console.WriteLine("No");
+            }
+            else Console.WriteLine("No");
+        }
+    }
+}
---
+++ part2_20/part2_20/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace part2_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string k = Console.ReadLine();
+            int number =int.Parse(k);
+            Console.WriteLine(Estimation(number));
+        }
+        static string Estimation(int number)
+        {
+            string text=null;
+            while (number > 0)
+            {
+               
+                if (number % 2==0)
+                {
+                    text = text + "0";
+                }
+                number = number /2;
+            }
+
+            return text;
+        }
+
+    }
+}
