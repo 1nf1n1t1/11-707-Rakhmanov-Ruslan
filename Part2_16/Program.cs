541148c780f6754f78b9a0efbd32a8ad5c319624
1nf1n1t1 <ruslan-3100@mail.com>
2017-10-13 11:21:08 +0300

    *

---
+++ Part2_16/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part2_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,number;
+            long maxlong,count=0,maxcount=0;
+            maxlong = long.Parse(Console.ReadLine());
+            for (i = 1; i <= maxlong; i++)
+            {
+               
+                number = i;
+                while (number!=0)
+                { if (maxcount < count) maxcount = count;
+                  if (number % 2 == 0)
+                  {
+                     count++;
+                     number = number / 2;
+                  }
+                  else
+                  {
+                      count = 0;
+                      number = number / 2;
+                  }
+
+                }
+               
+                count = 0;
+               
+            }
+            Console.WriteLine(maxcount);
+            
+        }
+    }
+}
