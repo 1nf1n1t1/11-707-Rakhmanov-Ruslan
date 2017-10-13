7b1b68fcb701284a93a36575b34f6585b3b8e73b
1nf1n1t1 <ruslan-3100@mail.com>
2017-10-13 12:49:40 +0300

    *

---
+++ Part3_8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long a1, a2, a3, t=0, n,i;
+            n = long.Parse(Console.ReadLine());
+            a1 = long.Parse(Console.ReadLine());
+            a2 = long.Parse(Console.ReadLine());
+            
+            for (i = 1; i <= n - 2; i++)
+            {
+                a3 = long.Parse(Console.ReadLine());
+                if ((a1 > a2) && (a2 < a3)) t++;
+                if ((a1 < a2) && (a2 > a3)) t++;
+                a1 = a2;
+                a2 = a3;
+
+            }
+            if (t == n - 2)  Console.WriteLine("Пилообразная");
+                else Console.WriteLine("Не пилообразная");
+        }
+
+        
+       
+    }
+}
